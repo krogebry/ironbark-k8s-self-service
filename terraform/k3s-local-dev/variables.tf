f6c5c91d557848c9a35655f7fe33722d7ba84ef0
Bryan Kroger <bryan.kroger@gmail.com>
2024-02-26 08:47:04 -0500

    ISSUE-31: Adding TF bits for docker registry and tailscale operator. (#38)
    Co-authored-by: bryan.kroger <bryan.kroger@renovo.auto>

--- terraform/k3s-local-dev/variables.tf
+++ terraform/k3s-local-dev/variables.tf
@@ -1,4 +1,12 @@
 
+variable "clientId" {
+  sensitive = true
+}
+
+variable "clientSecret" {
+  sensitive = true
+}
+
 data "kubernetes_namespace_v1" "kube-system" {
   metadata {
     name = "kube-system"
